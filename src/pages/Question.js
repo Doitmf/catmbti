3c7b6f790d12dd364340cfb19547b483b66bbb80
Doitmf <hodo5700@gmail.com>
2022-04-25 21:11:07 +0900

    add route, Home

---
+++ src/pages/Question.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class extends Component {
+  render() {
+    return (
+      <div>질문화면 입니다</div>
+    )
+  }
+}
---
+++ src/pages/Result.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Result extends Component {
+  render() {
+    return (
+      <div>결과화면입니다.</div>
+    )
+  }
+}
